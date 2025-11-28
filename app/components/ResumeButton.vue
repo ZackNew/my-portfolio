528fd9a585aa65ec77d4b63b1ccfed7545145201
zackNew <zekariyasmulugeta91@gmail.com>
2025-11-29 00:01:26 +0300

    feat: resume fix, and bg dots

--- app/components/ResumeButton.vue
+++ app/components/ResumeButton.vue
@@ -1,6 +1,6 @@
 <template>
   <a 
-    href="/resume.pdf" 
+    href="/Zekariyas_Mulugeta_Resume.pdf" 
     download="Zekariyas_Mulugeta_Resume.pdf"
     class="inline-flex items-center gap-2 text-primary-600 font-medium"
     :class="border ? 'transition-colors hover:bg-primary-50 border border-primary-600 px-4 py-2 duration-200 rounded-lg' : 'hover:text-primary-800'"
